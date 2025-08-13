4cf55643da76f1e8b14d8989e4187f6ce68eefe4
garzo94 <alexgarzo25@gmail.com>
2025-08-13 16:58:10 -0600

    SOLUTION 1.2

--- static/src/playground.js
+++ static/src/playground.js
@@ -1,15 +1,10 @@
 /** @odoo-module **/
 
 import { Component, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter"
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter }
 
-    setup(){
-        this.state = useState({ value:1 })
-    }
-
-    increment() {
-        this.state.value = this.state.value + 1;
-    }
 }
