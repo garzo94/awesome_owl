c5eb9fbe7093f4c0871aface3f2e340c53f77a2c
garzo94 <alexgarzo25@gmail.com>
2026-06-09 14:16:12 -0600

    1.14

--- static/src/card/card.js
+++ static/src/card/card.js
@@ -1,6 +1,6 @@
 /** @odoo-module */
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.Card";
@@ -14,4 +14,12 @@ export class Card extends Component {
             },
         }
     };
+
+    setup() {
+        this.state = useState({ isOpen: true });
+    }
+
+    toggleContent() {
+        this.state.isOpen = !this.state.isOpen;
+    }
 }
